8fc8284fe0bd421e5697b704fad8f032cc86e2cf
Студенческая <@rbiu.local>
2026-02-16 12:53:44 +0500

    task2

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,19 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public int $a = 1;
-    public int $b = 2;
+    public array $products = [
+        ['id' => 1, 'title' => 'Котик 1', 'price' => 250, 'path' => 'pict1.jpg'],
+        ['id' => 2, 'title' => 'Котик 2', 'price' => 8500, 'path' => 'pict2.jpg'],
+        ['id' => 3, 'title' => 'Котик 3', 'price' => 650, 'path' => 'pict3.jpg'],
+        ['id' => 4, 'title' => 'Котик 4', 'price' => 120, 'path' => 'pict4.jpg'],
+        ['id' => 5, 'title' => 'Котик 5', 'price' => 180, 'path' => 'pict5.jpg'],
+        ['id' => 6, 'title' => 'Котик 6', 'price' => 4500, 'path' => 'pict6.jpg'],
+        ['id' => 7, 'title' => 'Котик 7', 'price' => 3200, 'path' => 'pict7.jpg'],
+        ['id' => 8, 'title' => 'Котик 8', 'price' => 800, 'path' => 'pict8.jpg'],
+    ];
 
-
-    public function show1()
+    public function showIndex(): View
     {
-        $a = $this->a;
-        $b = $this->b;
-        $products =['яйца', 'мука', 'молоко', 'сахар', 'разрыхлитель'];
-        return view('second', compact('a', 'b', 'products'));
+        return view('home');
+    }
+
+    public function showArray(): View
+    {
+        return view('array', ['array' => $this->products]);
+    }
+
+    public function shuffleArray(): View
+    {
+        $shuffled = collect($this->products)->shuffle()->values()->all();
+        return view('array', ['array' => $shuffled]);
+    }
+
+    public function sortArray(): View
+    {
+        $sorted = collect($this->products)->sortBy('price')->values()->all();
+        return view('array', ['array' => $sorted]);
+    }
+
+    public function filterArray(): View
+    {
+        $filtered = collect($this->products)
+            ->filter(fn($p) => $p['price'] > 1000)
+            ->values()
+            ->all();
+        return view('array', ['array' => $filtered]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/second', [MainController::class, 'show1']);
+Route::get('/', [MainController::class, 'showIndex'])->name('home');
+Route::get('/array', [MainController::class, 'showArray'])->name('array');
+Route::get('/array/shuffle', [MainController::class, 'shuffleArray'])->name('array.shuffle');
+Route::get('/array/sort', [MainController::class, 'sortArray'])->name('array.sort');
+Route::get('/array/filter', [MainController::class, 'filterArray'])->name('array.filter');
